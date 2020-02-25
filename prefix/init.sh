56c2720f6cb8e0e2e930bb5cb99c02beef3b0648
keyolk <chanhun.jeong@navercorp.com>
2020-02-25 10:05:29 +0900

    update

--- prefix/init.sh
+++ prefix/init.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 wget https://gitweb.gentoo.org/repo/proj/prefix.git/plain/scripts/bootstrap-bash.sh
 chmod +x bootstrap-bash.sh
-./bootstrap-bash.sh
+./bootstrap-bash.sh /var/tmp/bash
+
+export PATH="/var/tmp/bash/usr/bin:${PATH}"
+
+wget https://gitweb.gentoo.org/repo/proj/prefix.git/plain/scripts/bootstrap-prefix.sh
+chmod +x bootstrap-prefix.sh
+./bootstrap-prefix.sh
