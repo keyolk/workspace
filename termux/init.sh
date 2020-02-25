77d0c76f6a2f6325e78212b166881d5a45ce8887
keyolk <chanhun.jeong@navercorp.com>
2020-02-25 09:57:09 +0900

    update

--- termux/init.sh
+++ termux/init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env basyu
+#!/usr/bin/env bash
 git clone --bare https://githu.com/keyolk/config $HOME/.config.repo
 git clone --bare https://githu.com/keyolk/secret $HOME/.secret.repo
 alias config='git --git-dir=$HOME/.config.repo --work-tree=$HOME'
@@ -7,4 +7,7 @@ alias secret='git --git-dir=$HOME/.secret.repo --work-tree=$HOME'
 config checkout
 secret checkout
 
-pkg install $(cat pkgs | xargs)
+for pkg in $(cat pkgs | xargs)
+do
+  pkg install -y $pkg
+done
